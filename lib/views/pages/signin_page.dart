e995286934c9eae97ffa8853bf47613f2e47e0d9
Abhin Pai <abhinpai96@gmail.com>
2022-04-15 03:15:16 -0400

    Added few pages

---
+++ lib/views/pages/signin_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SignInPage extends StatelessWidget {
+  const SignInPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(
+          child: Center(
+        child: Text("Sign In Paeg"),
+      )),
+    );
+  }
+}
